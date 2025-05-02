33bc0cc135fa9ad1a5d009e0400706c15cb935bd
viksito <viktor.martinov.p@gmail.com>
2025-05-02 12:55:31 +0100

    Adding Styles and Partials

--- app.js
+++ app.js
@@ -9,23 +9,33 @@ app.set("view engine", "ejs")
 app.listen(3000)
 
 app.get("/", (req, res) => { 
-    // res.send("<p>Home Page Here</p>")
-    res.render("index")
+    const blogs = [
+        {title: "Yoshi", snipet: "Come Home Yoshi"},
+        {title: "Mario", snipet: "I am going to save princess peach"},
+        {title: "Bowser", snipet: "I am going to have the pricess for myself"},
+    ];
+
+    res.render("index",  {
+        title: "Home",
+        blogs
+    })
 })
 
 app.get("/about", (req, res) => { 
     // res.send("<p>About Page Here</p>")
-    res.render("about")
-})
-
-app.get("/about-us", (req, res) => { 
-    res.redirect("/about")
+    res.render("about", { 
+        title: "About"
+    })
 })
 
 app.get("/blogs/create", (req, res) => {
-    res.render("create");
+    res.render("create", {
+        title: "Create a new blog"
+    });
 })
 
 app.use((req, res) => { 
-    res.status(404).render("404.ejs")
+    res.status(404).render("404.ejs", {
+        title: "404"
+    })
 })
---
+++ prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    tabWidth: 1,
+    requireConfig: true,
+    bracketSpacing: true,
+    bracketSameLine: false,
+    jsxBracketSameLine: false,
+    singleAttributePerLine: true,
+   }
