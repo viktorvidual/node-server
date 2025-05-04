ab3dc02fb5d84e630a97810cff44728099291f99
viksito <viktor.martinov.p@gmail.com>
2025-05-04 14:56:29 +0100

    Refactor application to use environment variables, add blog model, and enhance views with new routes and styles

--- app.js
+++ app.js
@@ -1,59 +1,85 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog'); // Remove `.ts` if it's being transpiled to JS
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
+const mongoDbUser = process.env.MONGO_DB_USER;
+const mongoDbPassword = process.env.MONGO_DB_PASSWORD;
 
-const mongoDbUser = "viktor"
-const mongoDbPassword="N9udQ45o2aVCr5O8"
+console.log(mongoDbPassword);
 
-//connect to mongo db
-const DB_URI = `mongodb+srv://${mongoDbUser}:${mongoDbPassword}@cluster0.ronxkun.mongodb.net/nodejs_blog?retryWrites=true&w=majority&appName=Cluster0`
+const DB_URI = `mongodb+srv://${mongoDbUser}:${mongoDbPassword}@cluster0.ronxkun.mongodb.net/nodejs_blog?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose.connect(DB_URI)
-.then((result) =>{
-    app.listen(3000)
-    console.log("connected to db")})
-.catch((er) => console.log(err))
+  .then(() => {
+    app.listen(3000);
+    console.log("connected to db");
+  })
+  .catch((err) => console.log(err));
 
+app.set("view engine", "ejs");
 
-//register view engie
-app.set("view engine", "ejs")
-// listen for requests
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev'));
+app.use(express.static("public"));
 
+app.get("/about", (_, res) => {
+  res.render("about", {
+    title: "About"
+  });
+});
 
-app.use(morgan('dev'))
+app.get("/", (_, res) => {
+  res.redirect("/blogs");
+});
 
-app.use(express.static("public"))
-
-app.get("/", (req, res) => { 
-    const blogs = [
-        {title: "Yoshi", snipet: "Come Home Yoshi"},
-        {title: "Mario", snipet: "I am going to save princess peach"},
-        {title: "Bowser", snipet: "I am going to have the pricess for myself"},
-    ];
-
-    res.render("index",  {
-        title: "Home",
+app.get("/blogs", (_, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then(blogs => {
+      res.render("index", {
+        title: "All Blogs",
         blogs
+      });
     })
+    .catch((e) => console.log(e));
+});
+
+app.get("/blogs/create", (_, res) => {
+  res.render("create", {
+    title: "Create a new blog"
+  });
+});
+
+app.get("/blogs/:id", (req, res) =>{ 
+  const id = req.params.id;
+  console.log(id);
+  Blog.findById(id)
+  .then(blog => { 
+    res.render('details', {
+      title: "Blog Details",
+      blog
+    })
+  })
+  .catch(e => console.log(e))
 })
 
-app.get("/about", (req, res) => { 
-    // res.send("<p>About Page Here</p>")
-    res.render("about", { 
-        title: "About"
-    })
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body)
+  blog.save()
+  .then((data) => { 
+    res.redirect("/blogs")
+  })
+  .catch(e => { 
+    console.log(e);
+  })
 })
 
-app.get("/blogs/create", (req, res) => {
-    res.render("create", {
-        title: "Create a new blog"
-    });
-})
-
-app.use((req, res) => { 
-    res.status(404).render("404.ejs", {
-        title: "404"
-    })
-})
+app.use((_, res) => {
+  res.status(404).render("404.ejs", {
+    title: "404"
+  });
+});
--- server.js
+++ server.js
@@ -1,53 +1,53 @@
-const http = require("http")
-const fs = require("fs")
-const _ = require("lodash")
+// const http = require("http")
+// const fs = require("fs")
+// const _ = require("lodash")
 
-const server = http.createServer((req, res) => { 
-    // lodash
-    const num = _.random(0, 20)
-    console.log("number", num);
+// const server = http.createServer((req, res) => { 
+//     // lodash
+//     const num = _.random(0, 20)
+//     console.log("number", num);
 
-    const greet = _.once(() => { 
-        console.log("hello");
-    }) 
+//     const greet = _.once(() => { 
+//         console.log("hello");
+//     }) 
     
-    greet()
-    greet()
+//     greet()
+//     greet()
     
-    let path = "./views"
+//     let path = "./views"
 
-    switch(req.url){
-        case "/":
-            res.statusCode = 200
-            path += "/index.html"
-            break;
-        case "/about":
-            res.statusCode = 200
-            path += "/about.html"
-            break;
-        case "/about-me":
-            res.statusCode = 301
-            res.setHeader("Location", "/about")
-            res.end();
-            break;
-        default:
-            res.statusCode = 404
-            path += "/404.html"
-            break;
-    }
+//     switch(req.url){
+//         case "/":
+//             res.statusCode = 200
+//             path += "/index.html"
+//             break;
+//         case "/about":
+//             res.statusCode = 200
+//             path += "/about.html"
+//             break;
+//         case "/about-me":
+//             res.statusCode = 301
+//             res.setHeader("Location", "/about")
+//             res.end();
+//             break;
+//         default:
+//             res.statusCode = 404
+//             path += "/404.html"
+//             break;
+//     }
 
-    res.setHeader("Content-Type", "text/html")
+//     res.setHeader("Content-Type", "text/html")
 
-    fs.readFile(path, (err, data) => { 
-        if(err){
-            console.log(err)
-        }
-        res.end(data)
-    })
+//     fs.readFile(path, (err, data) => { 
+//         if(err){
+//             console.log(err)
+//         }
+//         res.end(data)
+//     })
 
-})
+// })
 
-server.listen(3000, "localhost", () => { 
-    console.log("Server is running on http://localhost:3000")
-    console.log("Press Ctrl+C to stop the server")
-})
+// server.listen(3000, "localhost", () => { 
+//     console.log("Server is running on http://localhost:3000")
+//     console.log("Press Ctrl+C to stop the server")
+// })
