73e7682b753f582ca697993b915087b3bba59658
viksito <viktor.martinov.p@gmail.com>
2025-05-06 11:44:39 +0300

    MVC Arch

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog'); // Remove `.ts` if it's being transpiled to JS
 const dotenv = require('dotenv');
-const { log } = require('console');
+const blogRoutes = require("./routes/blogRoutes")
 
 dotenv.config();
 
 const app = express();
 const mongoDbUser = process.env.MONGO_DB_USER;
 const mongoDbPassword = process.env.MONGO_DB_PASSWORD;
-
-console.log(mongoDbPassword);
 
 const DB_URI = `mongodb+srv://${mongoDbUser}:${mongoDbPassword}@cluster0.ronxkun.mongodb.net/nodejs_blog?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -28,68 +25,17 @@ app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'));
 app.use(express.static("public"));
 
+app.get("/", (_, res) => {
+  res.redirect("/blogs");
+});
+
 app.get("/about", (_, res) => {
   res.render("about", {
     title: "About"
   });
 });
 
-app.get("/", (_, res) => {
-  res.redirect("/blogs");
-});
-
-app.get("/blogs", (_, res) => {
-  Blog.find().sort({ createdAt: -1 })
-    .then(blogs => {
-      res.render("index", {
-        title: "All Blogs",
-        blogs
-      });
-    })
-    .catch((e) => console.log(e));
-});
-
-app.get("/blogs/create", (_, res) => {
-  res.render("create", {
-    title: "Create a new blog"
-  });
-});
-
-app.get("/blogs/:id", (req, res) =>{ 
-  const id = req.params.id;
-  Blog.findById(id)
-  .then(blog => { 
-    res.render('details', {
-      title: "Blog Details",
-      blog
-    })
-  })
-  .catch(e => console.log(e))
-})
-
-app.delete("/blogs/:id", (req, res) => { 
-  const id = req.params.id;
-  
-  Blog.findByIdAndDelete(id)
-  .then(result => { 
-    console.log("item deleted");
-    res.json({
-      redirect: "/blogs"
-    })
-  })
-  .catch(e => console.log(e))
-})
-
-app.post("/blogs", (req, res) => {
-  const blog = new Blog(req.body)
-  blog.save()
-  .then((data) => { 
-    res.redirect("/blogs")
-  })
-  .catch(e => { 
-    console.log(e);
-  })
-})
+app.use("/blogs", blogRoutes)
 
 app.use((_, res) => {
   res.status(404).render("404.ejs", {
---
+++ controllers/blogController.js
@@ -0,0 +1,63 @@
+// blog_index, blog_details, blog_create_get, blog_create_post, blog_delete
+const Blog = require("../models/blog")
+
+const blog_index = (_, res) => { 
+    Blog.find().sort({ createdAt: -1 })
+    .then(blogs => {
+      res.render("blogs/index", {
+        title: "All Blogs",
+        blogs
+      });
+    })
+    .catch((e) => console.log(e));
+}
+
+const blog_details = (req, res) => { 
+    const id = req.params.id;
+    Blog.findById(id)
+    .then(blog => { 
+      res.render('blogs/details', {
+        title: "Blog Details",
+        blog
+      })
+    })
+    .catch(e => console.log(e))
+}
+
+const blog_create_get = (_, res) => {
+    res.render("blogs/create", {
+      title: "Create a new blog"
+    });
+}
+
+const blog_create_post = (req, res) => {
+    const blog = new Blog(req.body)
+    blog.save()
+    .then((data) => { 
+      res.redirect("/blogs")
+    })
+    .catch(e => { 
+      console.log(e);
+    })
+}
+
+const blog_delete = (req, res) => { 
+    const id = req.params.id;
+    
+    Blog.findByIdAndDelete(id)
+    .then(() => { 
+      console.log("item deleted");
+      res.json({
+        redirect: "/blogs"
+      })
+    })
+    .catch(e => console.log(e))
+}
+
+module.exports = { 
+    blog_index,
+    blog_details,
+    blog_create_get,
+    blog_create_post,
+    blog_delete
+}
